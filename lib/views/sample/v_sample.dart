a4b922d2069015ee0ddc615a59820c5c1dbc8ae8
Call Vin <kanggara75@gmail.com>
2022-04-15 16:41:26 +0700

    add: step 1

---
+++ lib/views/sample/v_sample.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/bloc/sample/sample_bloc.dart';
+
+class Sample extends StatelessWidget {
+  Sample({Key? key}) : super(key: key);
+
+  final CounterBloc bloc = CounterBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder(
+                stream: bloc.output,
+                initialData: bloc.counter,
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.data.toString(),
+                    style: const TextStyle(
+                      fontSize: 24,
+                    ),
+                  );
+                }),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                    onPressed: () {
+                      bloc.input.add("remove");
+                    },
+                    icon: const Icon(Icons.remove)),
+                IconButton(
+                    onPressed: () {
+                      bloc.input.add("add");
+                    },
+                    icon: const Icon(Icons.add)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
